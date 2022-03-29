dbb2e5a9abce31c865a81c4e9743e3283e46278b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-29 10:54:57 -0500

    done

---
+++ SectionTwo/Bus.java
@@ -0,0 +1,12 @@
+public class Bus{
+    public static void main(String[] args) {
+        int passengers = 0;
+        passengers += 9;
+        System.out.println(passengers);
+        passengers -= 5;
+        System.out.println(passengers);
+        passengers += 4;
+        System.out.println(passengers);
+        
+    }
+}
---
+++ SectionTwo/Variables.java
@@ -0,0 +1,8 @@
+public class Variables{
+    public static void main(String[] args){
+        int passenger = 20;
+
+        int busTickets = passenger;
+        System.out.println(busTickets);
+    }
+}
