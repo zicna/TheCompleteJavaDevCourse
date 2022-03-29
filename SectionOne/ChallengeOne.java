4595b065d6f68471567de19ac2fd6638884626f7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-29 06:42:24 -0500

    finished first challenge

---
+++ SectionOne/ChallengeOne.java
@@ -0,0 +1,20 @@
+// package SectionOne;
+
+public class ChallengeOne{
+
+    public static void main(String[] args) {
+        System.out.println("ZZZZZ");
+        System.out.println("    Z");
+        System.out.println("   Z ");
+        System.out.println("  Z  ");
+        System.out.println(" Z   ");
+        System.out.println("ZZZZZ");
+        System.out.println("     ");
+        System.out.println("M   M");
+        System.out.println("MM MM");
+        System.out.println("M M M");
+        System.out.println("M   M");
+        System.out.println("M   M");
+    }
+
+}
