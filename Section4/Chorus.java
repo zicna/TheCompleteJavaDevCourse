3741d495cb65fc30506859b50076e073b52e56ff
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-31 15:30:06 -0500

    worksop 4.1 done

---
+++ Section4/Chorus.java
@@ -0,0 +1,12 @@
+public class Chorus{
+    public static void main(String[] args) {
+        singChorus();
+    }
+
+    public static void singChorus(){
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on the good times");
+        System.out.println("Blame it on the boogie\n");
+    }
+}
---
+++ Section4/Detention.java
@@ -0,0 +1,20 @@
+public class Detention{
+    public static void main(String[] args) {
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+    }
+
+    public static void printLines(){
+        System.out.println("I will not copy and paste code.");
+        System.out.println("I will use Camel Case when writing class names.");
+        System.out.println("I will use lower Camel Case when writing function names.");
+        System.out.println("I will use lower Camel Case when writing variables names.\n");
+        System.out.println("I will use lower Camel Case when writing variables names.\n");
+        System.out.println("I will use lower Camel Case when writing variables names.\n");
+        System.out.println("I will use lower Camel Case when writing variables names.\n");
+    }
+}
