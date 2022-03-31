388814e580aba053a8a7ce4357f4a75a5c1a2171
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-31 00:52:34 -0500

    done

--- SectionThree/LogicalOperators.java
+++ SectionThree/LogicalOperators.java
@@ -1,5 +1,21 @@
 public class LogicalOperators{
     public static void main(String[] args) {
-        
+        int chemistryGrade = 67;
+        int englishGrade = 65;
+
+        if(chemistryGrade > 75 || englishGrade > 75){
+            System.out.println("Congratualitions, you got the scolarship!");
+        }else{
+            System.out.println("You did not get our scholarship");
+        }
+
+        int credits = 56;
+        double GPA = 3.2;
+
+        if(credits >= 40 && GPA >= 2.0){
+            System.out.println("You earnd your diploma!");
+        }else{
+            System.out.println("Sorry, you didn't earn your diploma!");
+        }
     }
 }
