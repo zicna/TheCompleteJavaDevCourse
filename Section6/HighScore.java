53b8fd0de9574acebd6a4126b591855440f57559
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-04 13:40:58 -0500

    workbook 6.6 done

--- Section6/HighScore.java
+++ Section6/HighScore.java
@@ -1,20 +1,22 @@
 public class HighScore{
     public static void main(String[] args) {
 
-        int[] scores = {randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt()};
+        int[] scores = {randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt()};
 
         for(int i = 0; i < scores.length; i++){
             System.out.print(" " + scores[i]);
         }
 
         int highScore = 0;
+        int possition = 0;
 
         for(int i = 0; i < scores.length; i++){
             if(highScore < scores[i]){
                 highScore = scores[i];
+                possition = i;
             }
         }
-        System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!");
+        System.out.println("\n\nThe highest score is: " + highScore + ".\n It's the gentlemen in seat: " + possition + " Give that man a cookie!");
         
     }
 
