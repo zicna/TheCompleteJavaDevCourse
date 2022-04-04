a53d741ab93510d305d5fd3c1831baab67b9675f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-04 13:35:48 -0500

    workbook 6.5 done

---
+++ Section6/HighScore.java
@@ -0,0 +1,24 @@
+public class HighScore{
+    public static void main(String[] args) {
+
+        int[] scores = {randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt(), randomInt()};
+
+        for(int i = 0; i < scores.length; i++){
+            System.out.print(" " + scores[i]);
+        }
+
+        int highScore = 0;
+
+        for(int i = 0; i < scores.length; i++){
+            if(highScore < scores[i]){
+                highScore = scores[i];
+            }
+        }
+        System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!");
+        
+    }
+
+    public static int randomInt(){
+        return (int)(Math.random() * 5001); // this should include zero up to 5000 => [0, 5000]
+    }
+}
