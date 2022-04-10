ce26b7f78d7ac4e7cb978172a87bff18f0ee84c8
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-10 17:20:37 -0500

    dispense machine done

--- Section7/challengeOne/Main.java
+++ Section7/challengeOne/Main.java
@@ -1,16 +1,20 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
 
+        Scanner scan  = new Scanner(System.in);
+        
         Item itemOne = new Item("Pepsi", 1.99, 3 );
-
+        
         Item[][] items = new Item[][] {
             { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
             { new Item("Fanta", 1.99, 2) , new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
             { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
         };
-
+        
         Machine machineOne = new Machine(items);
-
+        
         // * testing setters and getters
         // System.out.println(machineOne.getItem(2, 1));
         // Item item = machineOne.getItem(2, 1);
@@ -28,7 +32,24 @@ public class Main{
         System.out.println("***************************************************");
         System.out.println("\t WELCOME TO JAVA DRINKS!");
         System.out.println("***************************************************");
-        System.out.println(machineOne.toString()); 
+        
+        while(true){
+            System.out.println(machineOne); 
+            System.out.print("Pick a row: ");
+            int row = scan.nextInt();
+            System.out.print("Pick a spot in the row: "); 
+            int spot = scan.nextInt();
+            if(machineOne.dispense(row, spot)){
+                System.out.println("\nEnjoy your drink! Press 1 to purchase another: ");
+            }else{
+                System.out.println("\nSorry, we're out of this item. Press 1 to purchase another: ");
+            }
+            if(scan.nextInt() != 1){
+                break;
+            }
+        }
 
+        
+        // scan.close();
     }
 }
