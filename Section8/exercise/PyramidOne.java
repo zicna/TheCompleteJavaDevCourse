2da28b6a5b099e8ed9b9a10615fc0937a8b8158a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-11 10:28:28 -0500

    exercise six done

---
+++ Section8/exercise/PyramidOne.java
@@ -0,0 +1,12 @@
+public class PyramidOne{
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = rows; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
