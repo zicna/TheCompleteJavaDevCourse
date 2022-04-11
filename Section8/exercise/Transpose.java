e23e5679ee836e28f3b3e97287f4e4515e88eca8
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-11 14:41:45 -0500

    exercise 10 doen

---
+++ Section8/exercise/Transpose.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Transpose{
+    public static void main(String[] args) {
+        int temp = 0;
+        int[][] matrix = new int[][] {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[i].length; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        System.out.println();
+        printMatrix(matrix);
+    }
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+                System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
+}
