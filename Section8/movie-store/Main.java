7170e7a3bedba720afc2fc1b219946140381f4c3
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-14 12:22:16 -0500

    workbook movie-store all done

--- Section8/movie-store/Main.java
+++ Section8/movie-store/Main.java
@@ -1,23 +1,26 @@
 import models.Store;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import models.Movie;
 
 public class Main {
+
+    public static Store movieStore = new Store();
     public static void main(String[] args) {
         System.out.println("\n********************JAVA VIDEO STORE********************\n");
-
-        Store movieStore = new Store();
-        movieStore.addMovie(new Movie("The Godfather", "Blue-Ray", 9.1));
-        movieStore.addMovie(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2));
-        movieStore.addMovie(new Movie("The Godfather: Part II", "DVD", 9));
-
         
-        System.out.println(movieStore);
-        movieStore.action("The Godfather", "sell");
-        movieStore.action("The Shawshank Redemption", "rent");
-        
-        System.out.println(movieStore);
-        movieStore.action("The Shawshank Redemption", "return");
-        System.out.println(movieStore);
+        try {
+            loadMovies("movies.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }finally{
+            System.out.println("MOVIES LOADED\n\n");
+            System.out.println(movieStore);
+            manageMovies();
+        }
     }
 
     /**
@@ -31,6 +34,40 @@ public class Main {
      *   • 3. call close() from the Scanner object.
      */
 
+     public static void manageMovies(){
+         Scanner scan = new Scanner(System.in);
+         while(true){
+             System.out.println("\nWould you like to \n\ta) purchase\n\tb) rent \n\tc) return.");
+             String response = scan.nextLine();
+
+             if(! (response.equals("a") || response.equals("b") || response.equals("c"))){
+                 scan.close();
+                 break;
+             }
+
+             System.out.println("Enter the name of the movie: ");
+             String name = scan.nextLine();
+             if(movieStore.getMovie(name) == null){
+                System.out.println("\n\nThe input you provided is not valid. Please try again\n");
+                continue;
+             }
+
+             switch(response){
+                case "a": 
+                if(!movieStore.getMovie(name).isAvailable()){
+                    System.out.println("\n\n\nThe Movie is not avaiable for purchase. Please try again.");
+                    continue;
+                }
+                movieStore.action(name, "sell");
+                break;
+                case "b": movieStore.action(name,"rent" ); break;
+                case "c": movieStore.action(name,"return" ); break;
+             }
+             System.out.println("\n\nUPDATED MOVIES\n\n" + movieStore);
+        }
+        scan.close();
+     }
+
 
     /**
      * Name: loadMovies
@@ -43,4 +80,35 @@ public class Main {
      *        Hint: You will need to 'split' a String into three Strings.
      */
 
+     public static void loadMovies(String fileName) throws FileNotFoundException{
+        FileInputStream fis = new FileInputStream(fileName);
+        Scanner scanFile = new Scanner(fis);
+        while(scanFile.hasNextLine()){
+            String firstLine = scanFile.nextLine();
+
+            String movieName = firstLine.split("--")[0];
+            String format = firstLine.split("--")[1];
+            double rating = Double.parseDouble(firstLine.split("--")[2]);
+            
+            movieStore.addMovie(new Movie(movieName, format, rating));
+        }
+        scanFile.close();
+     }
+
 }
+
+
+
+// Store movieStore = new Store();
+// movieStore.addMovie(new Movie("The Godfather", "Blue-Ray", 9.1));
+// movieStore.addMovie(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2));
+// movieStore.addMovie(new Movie("The Godfather: Part II", "DVD", 9));
+
+
+// System.out.println(movieStore);
+// movieStore.action("The Godfather", "sell");
+// movieStore.action("The Shawshank Redemption", "rent");
+
+// System.out.println(movieStore);
+// movieStore.action("The Shawshank Redemption", "return");
+// System.out.println(movieStore);
--- Section8/movie-store/models/Store.java
+++ Section8/movie-store/models/Store.java
@@ -13,6 +13,15 @@ public class Store{
         return new Movie(movies.get(index));
     }
 
+    public Movie getMovie(String name){
+        for (int i = 0; i < movies.size(); i++) {
+            if(this.movies.get(i).getName().equals(name)){
+                return new Movie(this.movies.get(i));
+            }
+        }
+        return null;
+    }
+
     public void setMovie(int index, Movie movie){
         movies.set(index, new Movie(movie));
     }
@@ -22,14 +31,27 @@ public class Store{
     }
 
     public void action(String name, String action){
+        if(movies.isEmpty()){
+            throw new IllegalStateException("Store not in valid state to perform action");
+        }
+        if(!(action.equals("sell") || action.equals("rent") || action.equals("return"))){
+            throw new IllegalArgumentException("action must be sell, rent or return.");
+        }
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException("Name cannot be blank or null");
+        }
         for (int i = 0; i < movies.size(); i++) {
             if(movies.get(i).getName().equals(name)){
-                if(action.equals("sell")){
-                    movies.remove(i);
-                }else if(action.equals("rent")){
-                    movies.get(i).setIsAvaiable(false);
-                }else if(action.equals("return")){
-                    movies.get(i).setIsAvaiable(true);
+                switch(action){
+                    case "sell": 
+                        if(!movies.get(i).isAvailable()){
+                            throw new IllegalStateException("cannot sell movie theat is not avaiable");
+                        }
+                    movies.remove(i);break;
+                    case "rent": 
+                        movies.get(i).setIsAvaiable(false);break;
+                    case "return":
+                        movies.get(i).setIsAvaiable(true);break;
                 }
             }
         }
