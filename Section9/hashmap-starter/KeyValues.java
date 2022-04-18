f069410591c0b484da6729ebea716467cd9c093a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-18 17:04:46 -0500

    hashmap lesson done

--- Section9/hashmap-starter/KeyValues.java
+++ Section9/hashmap-starter/KeyValues.java
@@ -1,8 +1,26 @@
+import java.util.HashMap;
+
 public class KeyValues {
   
     public static void main(String[] args) {
         String[] vegetables = new String[] {"Cauliflower", "Spaghetti Squash", "Parsley"};
         double[] prices = new double[] {4.99, 1.99, 6.99};
+
+        HashMap <String, Double> items = new HashMap<String, Double>();
+
+        for (int i = 0; i < 3; i++) {
+            items.put(vegetables[i], prices[i]);
+            
+        }
+        items.forEach((key, value) -> System.out.println(key + ": " + value));
+        items.put("Parsley", 1.99);
+
+        items.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        items.get("Cauliflower");
+        System.out.println(items.get("Cauliflower"));
+        System.out.println(items.get("Spaghetti Squash"));
+        System.out.println(items.get("Parsley"));
     }
   
 }
