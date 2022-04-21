9ff8e6d37ae07cddc1b3107d922ad0ba1e96b1a1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-20 22:50:26 -0500

    added for-each loop

---
+++ RandomJava/EnhancedLoop.java
@@ -0,0 +1,11 @@
+public class EnhancedLoop{
+
+    public String[] words;
+
+    public EnhancedLoop(){
+        words = new String[]{"hello", "world", "and", "we", "are", "back"};
+    }
+
+
+
+}
---
+++ RandomJava/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        EnhancedLoop obj = new EnhancedLoop();
+
+        for(String word : obj.words){
+            System.out.println(word + "\n");
+        }
+    }
+}
