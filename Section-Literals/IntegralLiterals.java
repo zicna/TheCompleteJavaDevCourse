8273b08bcbc880b6428a4a6b030f1a92746ad99b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-05-26 11:56:36 -0500

    add integral literals

---
+++ Section-Literals/IntegralLiterals.java
@@ -0,0 +1,24 @@
+public class IntegralLiterals{
+    public static void main(String[] args) {
+        // * Decimal literals
+
+        int decimalInt = 12;
+
+        // *Octal literals
+
+        int octalInt = 014;
+
+        // *Hexadecimal literals
+
+        int hexaInt = 0xc;
+
+        // *Binary literals
+
+        int binInt = 0b1100;
+
+        System.out.println("integer used decimal literal " + decimalInt);
+        System.out.println("integer used octal literal " + octalInt);
+        System.out.println("integer used hexadecimal literal " + hexaInt);
+        System.out.println("integer used binary literal " + binInt);
+    }
+}
